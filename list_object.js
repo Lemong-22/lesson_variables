da1f95e6769a09cc1380f64f3722890b91e59729
yosia <yosiaherlianto@gmail.com>
2025-04-29 22:06:30 +0700

    latihan LIST and OBJECT

---
+++ list_object.js
@@ -0,0 +1,19 @@
+const alist = {
+    merk : 'totot',
+    type : 'B',
+    harga : 100,
+    jenis : {
+        a : 100,
+        b : 200
+    },
+
+};
+
+// for (let hartot = alist.harga; hartot<=1000; hartot+=100){
+//     console.log("harga totalnya adlaah : " , hartot);
+// };
+
+// delete alist.jenis
+// for (let key in alist){
+//     console.log(key , ": ", alist[key]);
+// };
