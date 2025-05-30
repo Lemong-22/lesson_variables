0762e2b579435341e27547b22e31c66317639ab4
yosia <yosiaherlianto@gmail.com>
2025-05-30 21:34:13 +0700

    Game tebak angka promax

--- tebak_angka.js
+++ tebak_angka.js
@@ -4,29 +4,53 @@ const rl = readline.createInterface({
     input : process.stdin,
     output : process.stdout
 });
+console.clear
+console.log("=====GAME TEBAK ANGKA By YOSIA======")
 
-let randomnum = Math.floor(Math.random()*6);
-let nyawa = 0
+const percobaan = 3
+let nyawa = percobaan
 
-console.clear()
-console.log("========GAME TEBAK ANGKA BY Yosia ========")
-
-function nanya (){
-    rl.question("TEBAK ANGKA 1-5 : ", cek)}
-
-function cek(input){
-    let angka = parseInt(input);
-    if (nyawa==2){
-        console.log(`Kesempatan menjawab habis, jawabannya adalah ${randomnum}`);
-        rl.close()
-    } else if (angka!=randomnum){
-        nyawa+=1
-        console.log(`SALAH, kesempatan menjawab ${nyawa}/3`)
-        nanya()
-    } else if (angka==randomnum){
-        console.log(`BENAR!, jawabannya adalah ${randomnum}`)
-        rl.close()
-    };
+function tanya(){
+    rl.question("tebak angka 1-5 : ", cek)
 };
 
-nanya()
+function tanyaulang(){
+    rl.question("lanjut? (y/n)", lanjut)
+    function lanjut(a){
+        if (a=="y"){
+            nyawa=percobaan
+            tanya()
+        } else {
+            console.log("terimakasih sudah bermain")
+            rl.close()
+        }
+    }
+}
+
+
+function cek(input){
+    let angkaRandom = Math.floor(Math.random()*6)
+    let jawaban = parseInt(input)
+    if (nyawa==0){
+        console.log("kesempatan menjawab habis anda gagal")
+        tanyaulang()
+    }else if (jawaban!=angkaRandom){
+        nyawa-=1
+        console.log(`jawaban salah kesempatan menjawab ${nyawa}/3`)
+        tanya()
+    }else{
+        console.log(`selamat anda benar jawabannya adalah ${angkaRandom}`)
+        tanyaulang()
+    }
+};
+
+tanya();
+//ngeribetin pake looping padahal max nyawa nya bisa langsung aja di set wkwk
+            //     while(nyawa<3){
+            //         nyawa++
+            //     }
+            //     tanya()
+            // } else{
+            // nyawa+=3
+            // tanya()
+            // }
